5c88a34d7977dbcb871f35bc78d5cd35643767e6
DFIT Vasia <pronin_lek@mail.ru>
2023-01-15 21:34:37 +0300

    Пример 6 Готов

--- Example007 Base/Program.cs
+++ Example007 Base/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = 1;
+int B = 2;
+int C = 6;
+int D = 8;
+int E = 4;
+
+int max = A;
+if(A > max) max = A;
+if(B > max) max = B;
+if(C > max) max = C;
+if(D > max) max = D;
+if(E > max) max = E;
+
+Console.Write("max = ");
+Console.WriteLine(max);
